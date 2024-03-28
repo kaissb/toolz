0d1795c5731811b956981a38241141c09d2cad1c
Kaiss Bouali <k.bouali@outlook.com>
2024-03-28 03:34:00 +0000

    more debugging

--- param/middleware.py
+++ param/middleware.py
@@ -2,7 +2,7 @@ def debug_cors(get_response):
     def middleware(request):
         print("Origin: %s", request.headers.get("Origin"))
         response = get_response(request)
-        print("AC Header: %s", request.get("Access-Control-Allow-Origin"))
+        print("AC Header: %s", request.headers.get("Access-Control-Allow-Origin"))
         return response
 
     return middleware
